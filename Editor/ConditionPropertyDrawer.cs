49934bcacbc7d3df079cdf6d8bac0d5744df0051
EZ <ezgaming3377@gmail.com>
2024-06-01 02:12:23 -0400

    Launch of v1.0.0-beta.1: Formatted as a package

---
+++ Editor/ConditionPropertyDrawer.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using EZConditions;
+
+namespace EZConditions
+{
+    [CustomPropertyDrawer(typeof(Condition))]
+    public class ConditionPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Display ReadOnly Info
+            try
+            {
+                EditorGUI.BeginProperty(position, label, property);
+                int nativeIndent = EditorGUI.indentLevel;
+                EditorGUI.indentLevel = 0;
+
+                /* No longer needed since Condition has a ToString Method
+                SerializedProperty obj = property.FindPropertyRelative("obj");
+                SerializedProperty function = property.FindPropertyRelative("methodName");
+                SerializedProperty comparator = property.FindPropertyRelative("comparator");
+                SerializedProperty param2 = property.FindPropertyRelative("param2Value");
+                SerializedProperty param2AsObject = property.FindPropertyRelative("param2AsUnityObject");
+                SerializedProperty OR = property.FindPropertyRelative("or");
+
+                string orText = (OR.boolValue) ? "OR" : "AND";
+                string param2String = (string.IsNullOrEmpty(param2.stringValue)) ? "Null" : param2.stringValue; 
+
+                // A super simple version just telling you what conditions you've made. I will make it editable and pretty later
+                if (param2AsObject.objectReferenceValue == null)
+                {
+                    EditorGUI.LabelField(position, $"{obj.objectReferenceValue.name}.{function.stringValue} Is" +
+                        $" {comparator.enumDisplayNames[comparator.enumValueIndex]} {param2String} {orText}");
+                }
+                else
+                {
+                    EditorGUI.LabelField(position,$"{obj.objectReferenceValue.name}.{function.stringValue} Is " +
+                        $"{comparator.enumDisplayNames[comparator.enumValueIndex]} " +
+                        $"{param2AsObject.objectReferenceValue} {orText}");
+                }
+                */
+
+                if (property.boxedValue is Condition condition)
+                {
+                    // Display warning if invalid
+                    if (condition.Function.ReturnType == typeof(void))
+                    {
+                        EditorGUI.HelpBox(position, $"{condition} always returns false", MessageType.Warning);
+                    }
+                    else
+                    {
+                        EditorGUI.LabelField(position, $"{condition}");
+                    }
+                }
+
+                EditorGUI.indentLevel = nativeIndent;
+                EditorGUI.EndProperty();
+
+            }
+            catch (System.Exception)
+            {
+                Debug.LogError("Failed to build Custom Conditions Property. Building Default Look");
+                base.OnGUI(position, property, label);
+            }
+        }
+
+    }
+
+}
---
+++ Runtime/ConditionManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZConditions;
+
+namespace EZConditions
+{
+    public class ConditionManager : MonoBehaviour
+    {
+        /// <summary>
+        /// List of conditions
+        /// </summary>
+
+        public List<Condition> Conditions = new List<Condition>();
+
+        public bool EvaluateConditions(bool debug = false)
+        {
+            bool totalLogic = true;
+            bool evaluatingOrGroup = false;
+            bool orGroupResult = false;
+
+            // Loop through all conditions and check their validity
+            // If any condition returns false, this whole thing is false. Otherwise,
+            // We return true
+            // Conditions are evaluated with OR Superiority. Meaning A*B+C*D+E results in A*(B+C)*(D+E)
+            for (int i = 0; i < Conditions.Count; i++)
+            {
+                // Short circuit if at any point, our running total is false
+                if (!totalLogic) { break; }
+
+                Condition condition = Conditions[i];
+                bool currentResult = condition;
+
+                // If this condition is an OR condition, Enable orGroupLogic
+                if (condition.OR)
+                {
+                    evaluatingOrGroup = true;
+                    // the total of the orGroup so far is true or whatever this condition's result is
+                    orGroupResult = orGroupResult || currentResult;
+
+                    if (debug)
+                    {
+                        Debug.Log($"OR Condition: {condition} evaluated to {currentResult}. Current OR Group Result: {orGroupResult}");
+                    }
+
+                    // If it's the last condition in our list, finalize the OR group evaluation
+                    if (i == Conditions.Count - 1)
+                    {
+                        totalLogic = totalLogic && orGroupResult;
+                    }
+                }
+                else
+                {
+                    // If this is an AND condition mixed with preceding OR conditions
+                    if (evaluatingOrGroup)
+                    {
+                        // Finalize the OR group evaluation
+                        totalLogic = totalLogic && (orGroupResult || currentResult);
+                        evaluatingOrGroup = false;
+                        orGroupResult = false;
+                    }
+                    else if (!currentResult)
+                    {
+                        totalLogic = currentResult;
+                        if (debug)
+                        {
+                            Debug.Log($"{condition} is False.");
+                        }
+                    }
+                }
+
+            }
+
+            if (debug)
+            {
+                Debug.Log("Coalesced Conditions: " + totalLogic);
+            }
+            return totalLogic;
+        }
+
+
+        /// <summary>
+        /// Important because of the nonSerializable information in Condition, we should invalidate
+        /// the non-serializable field so that they are reconstructed as needed
+        /// </summary>
+        private void OnValidate()
+        {
+            List<Condition> ToRemove = new List<Condition>();
+            foreach (Condition condition in Conditions)
+            {
+
+                if (!condition.IsValid)
+                {
+                    ToRemove.Add(condition);
+                    continue;
+                }
+                else
+                {
+                    // Worth defining a custom OnValidate for Condition
+                    condition.Reconstruct();
+                }
+            }
+
+            foreach (Condition condition in ToRemove)
+            {
+                Conditions.Remove(condition);
+                Debug.LogWarning("Invalid Condition Removed");
+            }
+        }
+    }
+}
+
